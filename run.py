bbf463f7bd880f62c3fa5a00a2f2f00966489a88
Ali <ali.kimov@narxoz.kz>
2026-04-21 02:34:54 +0500

    123

--- run.py
+++ run.py
@@ -99,11 +99,10 @@ def init_db():
 
 
 if __name__ == '__main__':
-    debug = os.environ.get('FLASK_DEBUG', 'true').lower() == 'true'
     socketio.run(
         app,
-        debug=debug,
         host='0.0.0.0',
         port=int(os.environ.get('PORT', 5051)),
-        allow_unsafe_werkzeug=debug,
+        debug=False,
+        allow_unsafe_werkzeug=True
     )
--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 if [ -z "$OPENAI_API_KEY" ]; then
-    export OPENAI_API_KEY=$(echo 'c2stcHJvai1OWURHOFdDcTN5ay16bXF2VEI1M3NpR20yU2N3Zkp2WElSbjM3bUs1QnA4QUhEY2dZZTR3LTVJOGFxaXg1SVVGdjNtcEJOT3FmQlQzQmxia0ZKZkFlOTlUVVZodHdSR1pfVWpFQUNjU0t6cEdsMXVhTVJ6TnZfa29HRENPY2pEekxvNXhwc0VKekFhcGZwX3FVMVNqNlN4a2xBb0E=' | base64 -d)
+    export OPENAI_API_KEY=$(echo 'c2stcHJvaj...' | base64 -d)
 fi
-exec gunicorn --worker-class geventwebsocket.gunicorn.workers.GeventWebSocketWorker -w 1 --bind 0.0.0.0:$PORT run:app
+
+python run.py
